30393b2e69f8dcf734d16d17e3ce4055104b872b
Richie Hao <haoruiqian@gmail.com>
2015-04-27 14:38:45 +0800

    Add api for sending email.

---
+++ settings.py
@@ -0,0 +1,12 @@
+
+BASE_URL = 'https://api.mailgun.net/v3/'
+DATETIME_FORMAT = '%a, %d %b %Y %H:%M:%S %Z'
+
+import logging
+stream_handler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+stream_handler.setFormatter(formatter)
+LOGGER_HANDLERS = [stream_handler]
+DEFAULT_LOGGER_LEVEL = 'INFO'
+
+from local_settings import *
