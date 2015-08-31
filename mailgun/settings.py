304fdb1d7816c16a46a1fc57e8eca56fc35f4a78
Richie Hao <richie.hao@blackboard.com>
2015-08-31 14:35:50 +0800

    move settings inside

--- mailgun/settings.py
+++ mailgun/settings.py
@@ -8,3 +8,10 @@ formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 stream_handler.setFormatter(formatter)
 LOGGER_HANDLERS = [stream_handler]
 DEFAULT_LOGGER_LEVEL = 'INFO'
+
+allowed_levels = ('INFO', 'WARNING', 'ERROR', 'DEBUG')
+
+def set_default_logger_level(level):
+    if level not in allowed_levels:
+        raise Exception('%s not in allowed levels "%s"' % (level, ','.join(allowed_levels)))
+    DEFAULT_LOGGER_LEVEL = level
